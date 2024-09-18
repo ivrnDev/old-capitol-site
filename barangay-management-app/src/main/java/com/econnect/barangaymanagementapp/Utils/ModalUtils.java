66fc27965ee5a9577cf001e3be5c4f0ca5e80cff
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-18 04:04:20 -0700

    feat: create a modal utility helper for reusability

---
+++ barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Utils/ModalUtils.java
@@ -0,0 +1,49 @@
+package com.econnect.barangaymanagementapp.Utils;
+
+import com.econnect.barangaymanagementapp.Controller.ModalController;
+import com.econnect.barangaymanagementapp.MainApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class ModalUtils {
+    private static Stage modalStage = null;
+
+    public static void showConfirmationModal(String header, String message, ModalCallback callback) {
+        //Prevent modal duplication
+        if (isModalShowing()) return;
+
+        try {
+            FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("View/Components/modal.fxml"));
+            Parent root = loader.load();
+            ModalController controller = loader.getController();
+            controller.setup(header, message, (isConfirmed) -> {
+                if (callback != null) {
+                    callback.onResult(isConfirmed);
+                }
+            });
+
+            modalStage = new Stage();
+            Scene scene = new Scene(root);
+
+            modalStage.initStyle(StageStyle.UNDECORATED);
+            modalStage.setScene(scene);
+            modalStage.showAndWait();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public interface ModalCallback {
+        void onResult(boolean isConfirmed);
+    }
+
+    private static boolean isModalShowing() {
+        return modalStage != null && modalStage.isShowing();
+    }
+}
