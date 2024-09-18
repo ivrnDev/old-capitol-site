7fda6dc659a6e63f23aefbe5b1a47c9606400226
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-18 04:04:45 -0700

    refactor: generalize the exports

--- barangay-management-app/src/main/java/module-info.java
+++ barangay-management-app/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.econnect.barangaymanagementapp {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.econnect.barangaymanagementapp to javafx.fxml;
+    opens com.econnect.barangaymanagementapp.Controller to javafx.fxml;
     exports com.econnect.barangaymanagementapp;
-    exports com.econnect.barangaymanagementapp.Controller;
 }
