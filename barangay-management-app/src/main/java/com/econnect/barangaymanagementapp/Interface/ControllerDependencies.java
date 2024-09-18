f8fc44786367e434f5dc75f11ff61b90efc0ae9d
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-18 05:57:44 -0700

    refactor: create a controller dependencies interface

---
+++ barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Interface/ControllerDependencies.java
@@ -0,0 +1,7 @@
+package com.econnect.barangaymanagementapp.Interface;
+
+import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
+
+public interface ControllerDependencies {
+    void setDependencies(DependencyInjector dependencyInjector);
+}
