4614f5236bc8f69413c54602bc9a9b1dd5daf78f
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-19 08:20:06 -0700

    fix: remove unused imports

--- barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Controller/LoginController.java
+++ barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Controller/LoginController.java
@@ -7,7 +7,6 @@ import com.econnect.barangaymanagementapp.Utils.ModalUtils;
 import com.econnect.barangaymanagementapp.Utils.SceneManager;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
