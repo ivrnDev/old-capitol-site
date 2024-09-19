ac131d260b1a129b65360d6c087fec82c3e6e5ac
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-19 08:18:04 -0700

    feat: create a basic authentication

--- barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Service/LoginService.java
+++ barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Service/LoginService.java
@@ -1,5 +1,7 @@
 package com.econnect.barangaymanagementapp.Service;
 
 public class LoginService {
-
+    public boolean login(String username, String password) {
+        return "hr".equals(username) && "hr".equals(password);
+    }
 }
