0d5075f87d0f28ebd56ffe18b54ff36296a482ab
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-19 08:16:52 -0700

    feat: include the login service

--- barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Utils/DependencyInjector.java
+++ barangay-management-app/src/main/java/com/econnect/barangaymanagementapp/Utils/DependencyInjector.java
@@ -1,15 +1,18 @@
 package com.econnect.barangaymanagementapp.Utils;
 
 import com.econnect.barangaymanagementapp.Interface.ControllerDependencies;
+import com.econnect.barangaymanagementapp.Service.LoginService;
 import javafx.stage.Stage;
 
 public class DependencyInjector {
     private final SceneManager sceneManager;
     private final ModalUtils modalUtils;
+    private final LoginService loginService;
 
     public DependencyInjector(Stage stage) {
         this.sceneManager = new SceneManager(stage, this);
         this.modalUtils = new ModalUtils(stage);
+        this.loginService = new LoginService();
     }
 
     public SceneManager getSceneManager() {
@@ -18,6 +21,10 @@ public class DependencyInjector {
 
     public ModalUtils getModalUtils() {
         return modalUtils;
+    }
+
+    public LoginService getLoginService() {
+        return loginService;
     }
 
     public void injectDependenciesToController(Object controller) {
