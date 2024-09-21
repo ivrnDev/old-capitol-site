0cc2b1c513122b723ccbc1d12e2166c06f5c48b5
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-21 00:48:08 -0700

    feat: create an employee domain

---
+++ src/main/java/com/econnect/barangaymanagementapp/Domain/Employee.java
@@ -0,0 +1,79 @@
+package com.econnect.barangaymanagementapp.Domain;
+
+import com.econnect.barangaymanagementapp.Enumeration.Departments;
+
+public class Employee {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String role;
+    private Departments department;
+
+    public Employee(String id, String firstName, String lastName, String username, String password, String role, Departments department) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.department = department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Departments getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Departments department) {
+        this.department = department;
+    }
+}
