c300a9bbdd60a6faae561062fe0a77dd0246961a
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-21 03:20:14 -0700

    refactor: use the implementation set header method

--- src/main/java/com/econnect/barangaymanagementapp/Controller/BarangayOffice/DashboardController.java
+++ src/main/java/com/econnect/barangaymanagementapp/Controller/BarangayOffice/DashboardController.java
@@ -1,25 +1,23 @@
 package com.econnect.barangaymanagementapp.Controller.BarangayOffice;
 
 import com.econnect.barangaymanagementapp.Controller.Base.HeaderBaseController;
-import com.econnect.barangaymanagementapp.Controller.Components.HeaderController;
 import com.econnect.barangaymanagementapp.Interface.ControllerDependencies;
 import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
 import com.econnect.barangaymanagementapp.Utils.UserSession;
 import javafx.fxml.FXML;
 
-public class DashboardController extends HeaderBaseController implements ControllerDependencies {
-    @FXML
-    private HeaderController headerController;
+public class DashboardController extends HeaderBaseController  implements ControllerDependencies {
+
     private UserSession userSession;
-    @FXML
-    public void initialize() {
-        headerController.setHeaderTitle("Barangay Office");
-        headerController.setGreetingText("Welcome, " + UserSession.getInstance().getCurrentEmployee().getFirstName());
-    }
 
     @Override
     public void setDependencies(DependencyInjector dependencyInjector) {
         this.userSession = dependencyInjector.getLoginService().getUserSession();
+    }
+
+    @FXML
+    public void initialize() {
+        setHeader("Barangay Office", "Welcome, " + userSession.getInstance().getCurrentEmployee().getFirstName());
     }
 
 }
--- src/main/java/com/econnect/barangaymanagementapp/Controller/HR/DashboardController.java
+++ src/main/java/com/econnect/barangaymanagementapp/Controller/HR/DashboardController.java
@@ -1,24 +1,24 @@
 package com.econnect.barangaymanagementapp.Controller.HR;
 
 import com.econnect.barangaymanagementapp.Controller.Base.HeaderBaseController;
-import com.econnect.barangaymanagementapp.Controller.Components.HeaderController;
 import com.econnect.barangaymanagementapp.Interface.ControllerDependencies;
 import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
 import com.econnect.barangaymanagementapp.Utils.UserSession;
 import javafx.fxml.FXML;
 
 public class DashboardController extends HeaderBaseController implements ControllerDependencies {
-    @FXML
-    private HeaderController headerController;
+
     private UserSession userSession;
-    @FXML
-    public void initialize() {
-        headerController.setHeaderTitle("Human Resource");
-        headerController.setGreetingText("Welcome, " + UserSession.getInstance().getCurrentEmployee().getFirstName());
-    }
 
     @Override
     public void setDependencies(DependencyInjector dependencyInjector) {
         this.userSession = dependencyInjector.getLoginService().getUserSession();
     }
+
+    @FXML
+    public void initialize() {
+        setHeader("Human Resource", "Welcome, " + userSession.getInstance().getCurrentEmployee().getFirstName());
+    }
+
+
 }
