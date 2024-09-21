9c892554df1955298b40720faa826bff9959e749
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-21 00:50:26 -0700

    feat: create an empty Employee Repository Implementation

---
+++ src/main/java/com/econnect/barangaymanagementapp/Repository/Employee/IEmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.econnect.barangaymanagementapp.Repository.Employee;
+
+import com.econnect.barangaymanagementapp.Domain.Employee;
+
+public interface IEmployeeRepository {
+//    Employee createEmployee(Employee employee);
+}
