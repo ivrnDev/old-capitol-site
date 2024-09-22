d6dce57924a1d138935928349de7048c12c2174d
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-22 00:53:11 -0700

    remove: remove ControllerDependencies.java

--- src/main/java/com/econnect/barangaymanagementapp/Interface/ControllerDependencies.java
+++
@@ -1,7 +0,0 @@
-package com.econnect.barangaymanagementapp.Interface;
-
-import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
-
-public interface ControllerDependencies {
-    void setDependencies(DependencyInjector dependencyInjector);
-}
