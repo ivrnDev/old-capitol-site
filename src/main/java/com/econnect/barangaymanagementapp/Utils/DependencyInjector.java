a48148d3d33c606e1764dcda1c855e84f8dd5b9b
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-23 06:01:42 -0700

    refactor: centralized the fxml loader

--- src/main/java/com/econnect/barangaymanagementapp/Utils/DependencyInjector.java
+++ src/main/java/com/econnect/barangaymanagementapp/Utils/DependencyInjector.java
@@ -1,14 +1,9 @@
 package com.econnect.barangaymanagementapp.Utils;
 
-import com.econnect.barangaymanagementapp.MainApplication;
 import com.econnect.barangaymanagementapp.Repository.Employee.EmployeeRepository;
 import com.econnect.barangaymanagementapp.Service.EmployeeService;
 import com.econnect.barangaymanagementapp.Service.LoginService;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class DependencyInjector {
     private final Stage stage;
@@ -18,18 +13,19 @@ public class DependencyInjector {
     private final UserSession userSession;
     private final NavigationState navigationState;
     private final SoundUtils soundUtils;
+    private final FXMLLoaderFactory fxmlLoaderFactory;
 
     public DependencyInjector(Stage stage) {
         this.stage = stage;
         this.soundUtils = new SoundUtils();
         this.navigationState = new NavigationState();
+        this.fxmlLoaderFactory = new FXMLLoaderFactory(this);
         this.sceneManager = new SceneManager(this);
         this.modalUtils = new ModalUtils(this);
         EmployeeRepository employeeRepository = new EmployeeRepository();
         EmployeeService employeeService = new EmployeeService(employeeRepository);
         this.userSession = UserSession.getInstance();
         this.loginService = new LoginService(employeeService, userSession);
-
     }
 
     public Stage getStage() {
@@ -60,27 +56,8 @@ public class DependencyInjector {
         return soundUtils;
     }
 
-    public FXMLLoader getLoader(String fxmlPath) {
-        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(fxmlPath));
-        loader.setControllerFactory(controllerClass -> injectDependenciesToController(controllerClass));
-        return loader;
+    public FXMLLoaderFactory getFxmlLoaderFactory() {
+        return fxmlLoaderFactory;
     }
-
-    private Object injectDependenciesToController(Class<?> controllerClass) {
-        try {
-            for (Constructor<?> constructor : controllerClass.getConstructors()) {
-                Class<?>[] paramTypes = constructor.getParameterTypes();
-
-                if (paramTypes.length == 1 && paramTypes[0] == DependencyInjector.class) {
-                    return constructor.newInstance(this);
-                }
-            }
-            return controllerClass.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException e) {
-            throw new RuntimeException("Failed to instantiate controller: " + controllerClass.getName(), e);
-        }
-    }
-
 
 }
