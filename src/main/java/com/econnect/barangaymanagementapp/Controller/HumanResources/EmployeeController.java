553fb96e9e63b862e416920a3888b5fa4aa737db
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-27 19:48:03 -0700

    refactor: add basic employee controller

--- src/main/java/com/econnect/barangaymanagementapp/Controller/HumanResources/EmployeeController.java
+++ src/main/java/com/econnect/barangaymanagementapp/Controller/HumanResources/EmployeeController.java
@@ -1,5 +1,30 @@
 package com.econnect.barangaymanagementapp.Controller.HumanResources;
 
-public class EmployeeController {
+import com.econnect.barangaymanagementapp.Utils.ButtonUtils;
+import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeController implements Initializable {
+
+    @FXML
+    private VBox content;
+
+    private final ButtonUtils buttonUtils;
+
+
+    public EmployeeController(DependencyInjector dependencyInjector) {
+        this.buttonUtils = dependencyInjector.getButtonUtils();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
 
 }
