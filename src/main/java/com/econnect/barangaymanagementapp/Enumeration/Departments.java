4a68b3bac444ff6a823bae5db3251a1b0daa3b39
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-28 20:11:47 -0700

    feat: provide a list of roles per department

--- src/main/java/com/econnect/barangaymanagementapp/Enumeration/Departments.java
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/Departments.java
@@ -3,21 +3,30 @@ package com.econnect.barangaymanagementapp.Enumeration;
 import java.util.List;
 
 import static com.econnect.barangaymanagementapp.Enumeration.NavigationItems.*;
+import static com.econnect.barangaymanagementapp.Enumeration.Roles.*;
 
 public enum Departments {
-    HUMAN_RESOURCES("Human Resources", "View/HumanResources/dashboard.fxml", List.of(DASHBOARD, ANALYTICS, EMPLOYEES)),
-    BARANGAY_OFFICE("Barangay Office", "View/BarangayOffice/dashboard.fxml", List.of(DASHBOARD, ANALYTICS, RESIDENTS, HISTORY)),
-    HEALTH_OFFICE("Health Office", "View/HealthOffice/dashboard.fxml", List.of(DASHBOARD, ANALYTICS)),
-    ;
+    HUMAN_RESOURCES(
+            "Human Resources",
+            "View/HumanResources/dashboard.fxml",
+            List.of(DASHBOARD, ANALYTICS, EMPLOYEES),
+            List.of(HR_MANAGER, HR_RECRUITER, HR_FRONT_DESK)),
+    BARANGAY_OFFICE(
+            "Barangay Office",
+            "View/BarangayOffice/dashboard.fxml",
+            List.of(DASHBOARD, ANALYTICS, RESIDENTS, HISTORY),
+            List.of(ADMINISTRATIVE_CLERK, OFFICE_FRONT_DESK, RECORDS_CLERK, FINANCIAL_CLERK, EVENT_COORDINATOR, CERTIFICATION_CLERK));
 
     private final String name;
     private final String link;
     private final List<NavigationItems> navigationItemsList;
+    private final List<Roles> roles;
 
-    Departments(String name, String link, List<NavigationItems> navigationItemsList) {
+    Departments(String name, String link, List<NavigationItems> navigationItemsList, List<Roles> roles) {
         this.name = name;
         this.link = link;
         this.navigationItemsList = navigationItemsList;
+        this.roles = roles;
     }
 
     public String getName() {
@@ -35,4 +44,9 @@ public enum Departments {
     public List<NavigationItems> getNavigationItems() {
         return navigationItemsList;
     }
+
+    public List<Roles> getRoles() {
+        return roles;
+    }
 }
+
