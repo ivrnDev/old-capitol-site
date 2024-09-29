834d100622db8acab19a31dd48941f1d63e0a60f
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-28 20:08:07 -0700

    feat: modify employee repo

--- src/main/java/com/econnect/barangaymanagementapp/Repository/Employee/IEmployeeRepository.java
+++ src/main/java/com/econnect/barangaymanagementapp/Repository/Employee/IEmployeeRepository.java
@@ -1,7 +1,21 @@
 package com.econnect.barangaymanagementapp.Repository.Employee;
 
+import com.econnect.barangaymanagementapp.DTO.EmployeeDTO;
 import com.econnect.barangaymanagementapp.Domain.Employee;
+import okhttp3.Response;
+
+import java.util.List;
 
 public interface IEmployeeRepository {
-//    Employee createEmployee(Employee employee);
+    Response createEmployee(Employee employee);
+
+    Employee updateEmployee(Employee employee);
+
+    void deleteEmployee(Employee employee);
+
+    Employee findEmployeeById(int id);
+
+    List<Employee> findAllEmployees();
+//    Employee findEmployeeByCredentials(String username, String password);
+
 }
