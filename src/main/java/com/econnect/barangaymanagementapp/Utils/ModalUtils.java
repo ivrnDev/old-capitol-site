13ab99f12a4080441f9d14a57e95a97adce42302
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-29 06:33:22 -0700

    refactor: add customize modal and make the code cleaner

--- src/main/java/com/econnect/barangaymanagementapp/Utils/ModalUtils.java
+++ src/main/java/com/econnect/barangaymanagementapp/Utils/ModalUtils.java
@@ -1,6 +1,8 @@
 package com.econnect.barangaymanagementapp.Utils;
 
+import com.econnect.barangaymanagementapp.Enumeration.CustomizeModal;
 import com.econnect.barangaymanagementapp.Enumeration.Modal;
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,6 +10,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -15,10 +18,10 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 public class ModalUtils {
-    private Stage modalStage = null;
     private final Stage parentStage;
     private final SoundUtils soundUtils;
     private final FXMLLoaderFactory fxmlLoaderFactory;
+    private Stage currentStage = null;
 
     public ModalUtils(DependencyInjector dependencyInjector) {
         this.parentStage = dependencyInjector.getStage();
@@ -31,27 +34,75 @@ public class ModalUtils {
     }
 
     public void showModal(Modal modal, String header, String message, Consumer<Boolean> callback) {
-        if (!isModalShowing()) {
-            try {
-                FXMLLoader loader = fxmlLoaderFactory.createFXMLLoader(modal.getFxmlPath(), modal, header, message, callback);
-                soundUtils.playSound(modal.getSound());
-                Parent root = loader.load();
-                modalStage = new Stage();
-                Scene scene = new Scene(root);
-                scene.setFill(Color.TRANSPARENT);
-                modalStage.initModality(callback == null ? Modality.NONE : Modality.APPLICATION_MODAL);
-                modalStage.initStyle(StageStyle.TRANSPARENT);
-                modalStage.initOwner(parentStage);
-                modalStage.setScene(scene);
-                modalStage.showAndWait();
-            } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            FXMLLoader loader = fxmlLoaderFactory.createFXMLLoader(modal.getFxmlPath(), modal, header, message, callback);
+            soundUtils.playSound(modal.getSound());
+            Parent root = loader.load();
+            Stage modalStage = new Stage();
+            Scene scene = new Scene(root);
+            scene.setFill(Color.TRANSPARENT);
+
+            modalStage.initModality(callback == null ? Modality.NONE : Modality.APPLICATION_MODAL);
+            modalStage.initStyle(StageStyle.TRANSPARENT);
+            modalStage.initOwner(currentStage != null ? currentStage : parentStage);
+            modalStage.setScene(scene);
+
+            setupFadeTransition(root);
+            centerModal(modalStage);
+
+            modalStage.showAndWait();
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void customizeModal(CustomizeModal customizeModal) {
+        try {
+            FXMLLoader loader = fxmlLoaderFactory.createFXMLLoader(customizeModal.getFxmlPath());
+            Parent root = loader.load();
+            Stage modalStage = new Stage();
+            Scene scene = new Scene(root);
+            currentStage = modalStage;
+            scene.setFill(Color.TRANSPARENT);
+
+            modalStage.initOwner(parentStage);
+            modalStage.initStyle(StageStyle.TRANSPARENT);
+            modalStage.initModality(Modality.WINDOW_MODAL);
+            modalStage.setScene(scene);
+
+            setupFadeTransition(root);
+            centerModal(modalStage);
+
+            modalStage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setupFadeTransition(Parent root) {
+        root.setOpacity(0);
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(200), root);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.play();
+    }
+
+    private void centerModal(Stage modalStage) {
+        modalStage.setOnShown(event -> {
+            double centerX = parentStage.getX() + parentStage.getWidth() / 2 - modalStage.getWidth() / 2;
+            double centerY = parentStage.getY() + parentStage.getHeight() / 2 - modalStage.getHeight() / 2;
+            modalStage.setX(centerX);
+            modalStage.setY(centerY);
+        });
+    }
+
+
+    public void closeModal() {
+        if (currentStage != null) {
+            currentStage.close();
+            currentStage = null;
         }
     }
 
 
-    private boolean isModalShowing() {
-        return modalStage != null && modalStage.isShowing();
-    }
 }
