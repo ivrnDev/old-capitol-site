943af38cdeb0ee5a217a5066b03182e040f3698d
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-28 20:10:48 -0700

    test: testing add employee btn

--- src/main/java/com/econnect/barangaymanagementapp/Controller/HumanResources/EmployeeController.java
+++ src/main/java/com/econnect/barangaymanagementapp/Controller/HumanResources/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.econnect.barangaymanagementapp.Controller.HumanResources;
 
+import com.econnect.barangaymanagementapp.Service.EmployeeService;
 import com.econnect.barangaymanagementapp.Utils.ButtonUtils;
 import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -14,17 +16,50 @@ public class EmployeeController implements Initializable {
     @FXML
     private VBox content;
 
+    @FXML
+    private Button addEmployeeBtn;
+
     private final ButtonUtils buttonUtils;
+    private final EmployeeService employeeService;
 
 
     public EmployeeController(DependencyInjector dependencyInjector) {
         this.buttonUtils = dependencyInjector.getButtonUtils();
+        this.employeeService = dependencyInjector.getEmployeeService();
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        addEmployeeBtn.setOnAction(e -> {
+//            Employee newEmployee = new Employee(
+//                    "12345",                           // id
+//                    "Ivan Ren",                            // firstName
+//                    "Villamora",                             // lastName
+//                    "Secretary",               // position
+//                    "johndoe@example.com",             // email
+//                    "123-456-7890",                    // contactNumber
+//                    "123 Main St, Anytown, USA",       // address
+//                    Gender.MALE,                       // gender
+//                    Roles.HR_MANAGER,                       // role
+//                    "ivanren",                         // username
+//                    "ivanren",                     // access
+//                    Status.EmployeeStatus.ACTIVE,             // status
+//                    Departments.HUMAN_RESOURCES,                    // department
+//                    LocalDateTime.now(),               // createdAt
+//                    LocalDateTime.now(),               // updatedAt
+//                    LocalDateTime.now().minusDays(1)   // lastLogin
+//            );
+//
+//            Response created = employeeService.createEmployee(newEmployee);
+//
+//            System.out.println("Employee created: " + created);
+
+//            for (EmployeeDTO employee : employees) {
+//                System.out.println(employee);
+//            }
+        });
+
 
     }
-
-
 }
