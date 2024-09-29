b6cff5d696787067fc93ee8f89eab3befe1a82af
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-28 20:06:35 -0700

    feat: create status enum

---
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/Status.java
@@ -0,0 +1,18 @@
+package com.econnect.barangaymanagementapp.Enumeration;
+
+public class Status {
+
+    public enum EmployeeStatus {
+        PENDING_APPROVAL, //Encoding  of new employee
+        EVALUATION, //
+        ACTIVE,
+        INACTIVE,
+        RESIGNED,
+        RETIRED,
+        REJECTED,
+        WITHDRAWN
+    }
+
+}
+
+
