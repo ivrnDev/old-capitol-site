aafd2ae20233aa69aea583adce6fb5615dfd5b6b
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-01 06:53:40 -0700

    refactor: throw exception if firebase url is not set

--- src/main/java/com/econnect/barangaymanagementapp/Config/Config.java
+++ src/main/java/com/econnect/barangaymanagementapp/Config/Config.java
@@ -2,7 +2,12 @@ package com.econnect.barangaymanagementapp.Config;
 
 public class Config {
     public static String getFirebaseUrl() {
-        return System.getenv("FIREBASE_URL");
-    }
+        String firebaseUrl = System.getenv("FIREBASE_URL");
 
+        if (firebaseUrl == null || firebaseUrl.isEmpty()) {
+            throw new IllegalArgumentException("FIREBASE_URL is not set");
+        }
+
+        return firebaseUrl;
+    }
 }
