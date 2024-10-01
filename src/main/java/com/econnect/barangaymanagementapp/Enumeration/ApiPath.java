81da27f09fbab62a3a9b8ac7ad5a459a7026f86d
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-30 18:18:03 -0700

    feat: create an api path for rest endpoints

---
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/ApiPath.java
@@ -0,0 +1,28 @@
+package com.econnect.barangaymanagementapp.Enumeration;
+
+public enum ApiPath {
+    EMPLOYEES("/3Employees"),
+    RESIDENTS("/3Residents"),
+    VISITORS("/3Visitors"),
+    ANNOUNCEMENTS("/3Announcements"),
+    EVENTS("/3Events"),
+    BLOTTERS("/3Blotters"),
+    DOCUMENTS("/3Documents"),
+    EMPLOYEE_LOGS("/3EmployeeLogs"),
+    RESIDENT_LOGS("/3ResidentLogs"),
+    VISITOR_LOGS("/3VisitorLogs"),
+    ANNOUNCEMENT_LOGS("/3AnnouncementLogs"),
+    EVENT_LOGS("/3EventLogs"),
+    BLOTTER_LOGS("/3BlotterLogs"),
+    DOCUMENT_LOGS("/3DocumentLogs");
+
+    private final String path;
+
+    ApiPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
