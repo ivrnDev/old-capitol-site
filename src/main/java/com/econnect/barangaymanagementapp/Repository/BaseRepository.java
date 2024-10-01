e08e44e1e77587c63b755dd48f30958129790403
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-01 09:53:51 -0700

    feat: add create in base repository

--- src/main/java/com/econnect/barangaymanagementapp/Repository/BaseRepository.java
+++ src/main/java/com/econnect/barangaymanagementapp/Repository/BaseRepository.java
@@ -1,11 +1,15 @@
 package com.econnect.barangaymanagementapp.Repository;
 
 import com.econnect.barangaymanagementapp.Domain.BaseEntity;
+import com.econnect.barangaymanagementapp.Enumeration.Modal;
 import com.econnect.barangaymanagementapp.Utils.DependencyInjector;
 import com.econnect.barangaymanagementapp.Utils.HTTPClient;
 import com.econnect.barangaymanagementapp.Utils.JsonConverter;
+import com.econnect.barangaymanagementapp.Utils.ModalUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import java.io.IOException;
@@ -17,10 +21,32 @@ import java.util.Map;
 public abstract class BaseRepository<T> {
     protected final HTTPClient client;
     protected final JsonConverter jsonConverter;
+    private final ModalUtils modalUtils;
 
     public BaseRepository(DependencyInjector dependencyInjector) {
         this.client = dependencyInjector.getHttpClient();
         this.jsonConverter = dependencyInjector.getJsonConverter();
+        this.modalUtils = dependencyInjector.getModalUtils();
+    }
+
+    protected Response create(String apiUrl, T object) {
+        try {
+            Request request = new Request.Builder()
+                    .url(apiUrl + ".json")
+                    .put(RequestBody.create(
+                            jsonConverter.convertObjectToJson(object), MediaType.parse("application/json")
+                    ))
+                    .build();
+            Response response = client.getClient().newCall(request).execute();
+            if (!response.isSuccessful()) {
+                modalUtils.showModal(Modal.ERROR, "Failed", "Failed to add employees. Please try again.");
+            }
+            return response;
+        } catch (IOException | IllegalArgumentException e) {
+            modalUtils.showModal(Modal.ERROR, "Error", "An error occurred while adding the employee.");
+            System.err.println("Unexpected Error: " + e.getMessage());
+            return null;
+        }
     }
 
     protected List<T> findAll(String apiUrl, TypeReference<Map<String, T>> typeReference) {
@@ -47,6 +73,7 @@ public abstract class BaseRepository<T> {
                     }
                 });
             }
+
             return new ArrayList<>(objectsMap.values());
         } catch (IOException e) {
             System.err.println("Unexpected Error: " + e.getMessage());
