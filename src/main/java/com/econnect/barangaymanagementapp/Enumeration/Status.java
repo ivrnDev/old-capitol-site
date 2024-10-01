3c74b6a21f26cbf37c77dadf35a9d0cb06ef190c
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-09-30 18:19:10 -0700

    feat: add name for status

--- src/main/java/com/econnect/barangaymanagementapp/Enumeration/Status.java
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/Status.java
@@ -3,14 +3,26 @@ package com.econnect.barangaymanagementapp.Enumeration;
 public class Status {
 
     public enum EmployeeStatus {
-        PENDING_APPROVAL, //Encoding  of new employee
-        EVALUATION, //
-        ACTIVE,
-        INACTIVE,
-        RESIGNED,
-        RETIRED,
-        REJECTED,
-        WITHDRAWN
+        PENDING_APPROVAL("Pending"), //Encoding  of new employee
+        EVALUATION("Evaluation"), //
+        ACTIVE("Active"),
+        INACTIVE("Inactive"),
+        RESIGNED("Resigned"),
+        RETIRED("Retired"),
+        REJECTED("Rejected"),
+        WITHDRAWN("Withdrawn"),
+        ;
+
+        private String status;
+
+        EmployeeStatus(String status) {
+            this.status = status;
+        }
+
+        public String getName() {
+            return status;
+        }
+
     }
 
 }
