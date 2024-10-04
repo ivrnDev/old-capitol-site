45f0bce897e040e24d79ed13d9a019ec6855e9aa
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-04 09:38:31 -0700

    feat: add 3-Resume directory to firebase storage

--- src/main/java/com/econnect/barangaymanagementapp/Enumeration/Paths/ImageDirectory.java
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/Paths/ImageDirectory.java
@@ -4,6 +4,7 @@ public enum ImageDirectory {
     PROFILE_PICTURE("/1x1Images", "1x1Images"),
     VALID_ID("/ValidIDs", "ValidIDs"),
     ANNOUNCEMENT("/announcement_images", "announcement_images"),
+    RESUME("/3-Resume", "Resume"),
     SAMPLE("/sample", "sample");
     public final String path;
     public final String name;
