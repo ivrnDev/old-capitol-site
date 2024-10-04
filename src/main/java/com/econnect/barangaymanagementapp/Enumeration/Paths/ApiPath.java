e35942da192c462de25f59aa6b715c269e4abab2
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-04 01:52:44 -0700

    refactor: fixed API path directory

--- src/main/java/com/econnect/barangaymanagementapp/Enumeration/Paths/ApiPath.java
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/Paths/ApiPath.java
@@ -1,20 +1,20 @@
 package com.econnect.barangaymanagementapp.Enumeration.Paths;
 
 public enum ApiPath {
-    EMPLOYEES("/3Employees"),
-    RESIDENTS("/3Residents"),
-    VISITORS("/3Visitors"),
-    ANNOUNCEMENTS("/3Announcements"),
-    EVENTS("/3Events"),
-    BLOTTERS("/3Blotters"),
-    DOCUMENTS("/3Documents"),
-    EMPLOYEE_LOGS("/3EmployeeLogs"),
-    RESIDENT_LOGS("/3ResidentLogs"),
-    VISITOR_LOGS("/3VisitorLogs"),
-    ANNOUNCEMENT_LOGS("/3AnnouncementLogs"),
-    EVENT_LOGS("/3EventLogs"),
-    BLOTTER_LOGS("/3BlotterLogs"),
-    DOCUMENT_LOGS("/3DocumentLogs");
+    EMPLOYEES("/3-Employees"),
+    RESIDENTS("/3-Residents"),
+    VISITORS("/3-Visitors"),
+    ANNOUNCEMENTS("/3-Announcements"),
+    EVENTS("/3-Events"),
+    BLOTTERS("/3-Blotters"),
+    DOCUMENTS("/3-Documents"),
+    EMPLOYEE_LOGS("/3-EmployeeLogs"),
+    RESIDENT_LOGS("/3-ResidentLogs"),
+    VISITOR_LOGS("/3-VisitorLogs"),
+    ANNOUNCEMENT_LOGS("/3-AnnouncementLogs"),
+    EVENT_LOGS("/3-EventLogs"),
+    BLOTTER_LOGS("/3-BlotterLogs"),
+    DOCUMENT_LOGS("/3-DocumentLogs");
 
     private final String path;
 
