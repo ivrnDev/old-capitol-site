39de48ead140a4f2b8950b37828777f1f68a0352
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-04 21:13:09 -0700

    feat: create a generic serializer to convert null values to empty string

---
+++ src/main/java/com/econnect/barangaymanagementapp/Config/Deserializer/GenericSerializer.java
@@ -0,0 +1,36 @@
+package com.econnect.barangaymanagementapp.Config.Deserializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class GenericSerializer<T> extends JsonSerializer<T> {
+    public GenericSerializer() {
+    }
+
+    @Override
+    public void serialize(T value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartObject();
+
+        Field[] fields = value.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);  // Allow access to private fields
+            try {
+                Object fieldValue = field.get(value);
+                // Convert null fields to empty strings, and handle non-string values appropriately
+                if (field.getType() == String.class) {
+                    gen.writeStringField(field.getName(), fieldValue != null ? (String) fieldValue : "");
+                } else {
+                    gen.writeObjectField(field.getName(), fieldValue);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace(); // Handle exceptions as needed
+            }
+        }
+
+        gen.writeEndObject();
+    }
+}
