5cbf44ada79863ee95dc78e57488ba13e6107348
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-04 21:11:40 -0700

    feat: add to string annotation

--- src/main/java/com/econnect/barangaymanagementapp/Domain/BaseEntity.java
+++ src/main/java/com/econnect/barangaymanagementapp/Domain/BaseEntity.java
@@ -4,11 +4,13 @@ package com.econnect.barangaymanagementapp.Domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @SuperBuilder
+@ToString
 @NoArgsConstructor
 public abstract class BaseEntity {
     private String id;
