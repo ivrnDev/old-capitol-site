09109a7555574a7e367cac9ae0e316d1c67eff44
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-05 03:48:23 -0700

    refactor: improve create button for general usage

--- src/main/java/com/econnect/barangaymanagementapp/Utils/ButtonUtils.java
+++ src/main/java/com/econnect/barangaymanagementapp/Utils/ButtonUtils.java
@@ -2,13 +2,18 @@ package com.econnect.barangaymanagementapp.Utils;
 
 import com.econnect.barangaymanagementapp.Enumeration.ButtonStyle;
 import com.econnect.barangaymanagementapp.Interface.Callback;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 
 public class ButtonUtils {
-    public Button createButton(String text, ButtonStyle buttonStyle, Callback callback) {
+    public static Button createButton(String text, ButtonStyle buttonStyle, Callback callback) {
         Button button = new Button(text);
+        button.setPrefWidth(70);
+        button.setPrefHeight(25);
+        button.setCursor(Cursor.HAND);
+        button.getStyleClass().add("button");
         button.getStyleClass().add(buttonStyle.getRootStyle());
-        button.setOnAction(event -> callback.onClicked());
+        button.setOnAction(_ -> callback.onClicked());
         return button;
     }
 }
