313ae6d52c3365998d2e7f931abdbb54ef4a90ed
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-06 07:11:14 -0700

    feat: create a view employee fxml

--- src/main/java/com/econnect/barangaymanagementapp/Enumeration/CustomizeModal.java
+++ src/main/java/com/econnect/barangaymanagementapp/Enumeration/CustomizeModal.java
@@ -1,7 +1,8 @@
 package com.econnect.barangaymanagementapp.Enumeration;
 
 public enum CustomizeModal {
-    ADD_EMPLOYEE("View/HumanResources/Modal/add-employee.fxml");
+    ADD_EMPLOYEE("View/HumanResources/Modal/add-employee.fxml"),
+    VIEW_EMPLOYEE("View/HumanResources/Modal/view-employee.fxml");
 
     private final String fxmlPath;
 
