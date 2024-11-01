b026bdbd57b53ab9854a4bcbce4a2531efcc7804
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-10-31 23:21:43 -0700

    feat: add date validator

--- src/main/java/com/econnect/barangaymanagementapp/util/Validator.java
+++ src/main/java/com/econnect/barangaymanagementapp/util/Validator.java
@@ -3,13 +3,20 @@ package com.econnect.barangaymanagementapp.util;
 import com.econnect.barangaymanagementapp.enumeration.modal.Modal;
 import com.econnect.barangaymanagementapp.util.ui.ModalUtils;
 import javafx.application.Platform;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
+import javafx.beans.value.ChangeListener;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.util.converter.DefaultStringConverter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.function.Predicate;
+
+import static com.econnect.barangaymanagementapp.enumeration.modal.Modal.ERROR;
 
 public class Validator {
     private final ModalUtils modalUtils;
@@ -25,13 +32,34 @@ public class Validator {
         return validatorType.getPredicate().test(inputField.getText());
     }
 
-    public boolean textFields(TextField[] textFields) {
+    public boolean checkBox(CheckBox[] checkBoxes) {
+        boolean hasSelected = false;
+        for (CheckBox checkBox : checkBoxes) {
+            if (checkBox.isSelected()) {
+                hasSelected = true;
+            }
+        }
+
+        if (hasSelected) {
+            return false;
+        }
+
+        if (hasError) {
+            errorTitle = "Failed";
+            errorMessage = "Please select at least one certificate to be issued";
+            triggerError();
+            return true;
+        }
+        return true;
+    }
+
+    public boolean hasEmptyFields(TextField[] textFields) {
         for (TextField textField : textFields) {
             if (textField.getText().isEmpty()) {
                 if (!hasError) {
                     hasError = true;
                     errorTitle = "Failed";
-                    errorMessage = "Please fill out all the fields";
+                    errorMessage = "Please fill out all required fields";
                 }
                 textField.setStyle("-fx-border-color: red");
             } else {
@@ -48,11 +76,44 @@ public class Validator {
         return false;
     }
 
-    private void triggerError() {
-        modalUtils.showModal(Modal.ERROR, errorTitle, errorMessage);
+    public boolean hasEmptyFields(TextField[] textFields, DatePicker[] datePickers) {
+        for (TextField textField : textFields) {
+            if (textField.getText().isEmpty()) {
+                if (!hasError) {
+                    hasError = true;
+                    errorTitle = "Failed";
+                    errorMessage = "Please fill out all required fields";
+                }
+                textField.setStyle("-fx-border-color: red");
+            } else {
+                hasError = false;
+                textField.setStyle("");
+            }
+            addTextFieldListener(textField);
+        }
+
+        for (DatePicker datePicker : datePickers) {
+            if (datePicker.getEditor().getText().isEmpty()) {
+                if (!hasError) {
+                    hasError = true;
+                    errorTitle = "Failed";
+                    errorMessage = "Please fill out all required fields";
+                }
+                datePicker.setStyle("-fx-border-color: red");
+            } else {
+                hasError = false;
+                datePicker.setStyle("");
+            }
+        }
+
+        if (hasError) {
+            triggerError();
+            return true;
+        }
+        return false;
     }
 
-    private void addTextFieldListener(TextField textField) {
+    public void addTextFieldListener(TextField textField) {
         textField.setOnKeyTyped(_ -> {
             if (!textField.getText().isEmpty()) {
                 textField.setStyle("");
@@ -60,6 +121,101 @@ public class Validator {
         });
     }
 
+    public void setupDatePicker(LocalDate minDate, LocalDate maxDate, DatePicker... datePickers) {
+        Arrays.stream(datePickers).forEach(datePicker -> {
+            datePicker.setDayCellFactory(picker -> new DateCell() {
+                @Override
+                public void updateItem(LocalDate date, boolean empty) {
+                    super.updateItem(date, empty);
+                    setDisable(date.isBefore(minDate) || date.isAfter(maxDate));
+                }
+            });
+            datePicker.getEditor().setTextFormatter(new TextFormatter<>(change -> {
+                String newText = change.getControlNewText();
+
+                if (newText.length() > 10 || !newText.matches("[\\d/]*")) {
+                    return null;
+                }
+
+                if (newText.length() >= 2) {
+                    String month = newText.substring(0, 2);
+                    if (Integer.parseInt(month) < 1 || Integer.parseInt(month) > 12) {
+                        return null;
+                    }
+                }
+
+                if (newText.length() >= 5) {
+                    String day = newText.substring(3, 5);
+                    if (Integer.parseInt(day) < 1 || Integer.parseInt(day) > 31) {
+                        return null;
+                    }
+                }
+
+                if (newText.length() == 10) {
+                    try {
+                        LocalDate date = LocalDate.parse(newText, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+                        if (date.isBefore(minDate) || date.isAfter(maxDate)) {
+                            return null;
+                        }
+                    } catch (DateTimeParseException e) {
+                        return null;
+                    }
+                }
+
+                int length = newText.length();
+                if (length == 2 || length == 5) {
+                    if (!newText.endsWith("/")) {
+                        newText = newText.substring(0, length) + "/" + newText.substring(length);
+                        change.setText(newText);
+                        change.setRange(0, change.getControlText().length());
+                    }
+                }
+
+                if (length < change.getControlText().length()) {
+                    if (length == 2 || length == 5) {
+                        newText = newText.substring(0, length - 1);
+                        change.setText(newText);
+                        change.setRange(0, change.getControlText().length());
+                    }
+                }
+
+                String finalNewText = newText;
+                Platform.runLater(() -> datePicker.getEditor().positionCaret(finalNewText.length()));
+
+                return change;
+            }));
+
+            datePicker.getEditor().focusedProperty().addListener((observable, oldValue, newValue) -> {
+                if (!newValue) {
+                    String text = datePicker.getEditor().getText();
+                    if (text.length() == 10) {
+                        try {
+                            LocalDate date = LocalDate.parse(text, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+                            if (date.isBefore(minDate) || date.isAfter(maxDate)) {
+                                throw new DateTimeParseException("Date out of range", text, 0);
+                            }
+                            datePicker.setValue(date);
+                        } catch (DateTimeParseException e) {
+                            datePicker.getEditor().setText("");
+                            datePicker.setValue(null);
+                            datePicker.setStyle("-fx-border-color: red;");
+                        }
+                    } else {
+                        datePicker.getEditor().setText("");
+                        datePicker.setValue(null);
+                        datePicker.setStyle("-fx-border-color: red;");
+                    }
+                } else {
+                    datePicker.setStyle(null);
+                }
+            });
+
+        });
+    }
+
+    private void triggerError() {
+        modalUtils.showModal(Modal.ERROR, errorTitle, errorMessage);
+    }
 
     // Methods that are separated from the original class functionality
     public void createResidentIdFormatter(TextField inputField) {
@@ -135,5 +291,6 @@ public class Validator {
     }
 
 }
+
 
 
