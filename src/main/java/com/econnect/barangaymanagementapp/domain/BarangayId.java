77b04961398e28322f7636c261d1eb14c08c6df2
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-11-01 22:54:50 -0700

    feat: add ignore json properties

--- src/main/java/com/econnect/barangaymanagementapp/domain/BarangayId.java
+++ src/main/java/com/econnect/barangaymanagementapp/domain/BarangayId.java
@@ -3,6 +3,7 @@ package com.econnect.barangaymanagementapp.domain;
 import com.econnect.barangaymanagementapp.config.deserializer.GenericSerializer;
 import com.econnect.barangaymanagementapp.enumeration.type.ApplicationType;
 import com.econnect.barangaymanagementapp.enumeration.type.StatusType.BarangayIdStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,7 @@ import java.time.ZonedDateTime;
 @AllArgsConstructor
 @JsonSerialize(using = GenericSerializer.class)
 @EqualsAndHashCode(callSuper = false)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BarangayId extends BaseEntity {
     private String height;
     private String weight;
