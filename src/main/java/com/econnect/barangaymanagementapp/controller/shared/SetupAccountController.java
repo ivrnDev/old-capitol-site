db099dd4c22a3e6c1adbb1e979c56cb29f5af8da
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-11-02 06:40:05 -0700

    refactor: improve validation logic

--- src/main/java/com/econnect/barangaymanagementapp/controller/shared/SetupAccountController.java
+++ src/main/java/com/econnect/barangaymanagementapp/controller/shared/SetupAccountController.java
@@ -6,6 +6,7 @@ import com.econnect.barangaymanagementapp.enumeration.type.DepartmentType;
 import com.econnect.barangaymanagementapp.enumeration.type.RoleType;
 import com.econnect.barangaymanagementapp.service.EmployeeService;
 import com.econnect.barangaymanagementapp.util.DependencyInjector;
+import com.econnect.barangaymanagementapp.util.Validator;
 import com.econnect.barangaymanagementapp.util.ui.LoadingIndicator;
 import com.econnect.barangaymanagementapp.util.ui.ModalUtils;
 import javafx.application.Platform;
@@ -13,6 +14,7 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import okhttp3.Response;
@@ -21,47 +23,44 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SetupAccountController implements BaseViewController {
-    private final ModalUtils modalUtils;
-    private final EmployeeService employeeService;
-    private DepartmentType selectedDepartment;
-    private String employeeId;
-
     @FXML
     private AnchorPane rootContainer;
-
     @FXML
     private Button confirmBtn, cancelBtn;
-
+    @FXML
+    private ImageView closeBtn;
     @FXML
     private ComboBox<String> departmentComboBox, roleComboBox;
+
+    private final ModalUtils modalUtils;
+    private final EmployeeService employeeService;
+    private final Validator validator;
+    private DepartmentType selectedDepartment;
+    private String residentId;
+
 
     public SetupAccountController(DependencyInjector dependencyInjector) {
         this.modalUtils = dependencyInjector.getModalUtils();
         this.employeeService = dependencyInjector.getEmployeeService();
+        this.validator = dependencyInjector.getValidator();
     }
 
     public void initialize() {
         setupActionButtons();
         populateDepartmentComboBox();
-        setupDepartmentComboBoxListener();
+        setupEventListener();
     }
 
     private void setupActionButtons() {
+        closeBtn.setOnMouseClicked(_ -> closeWindowConfirmation());
         cancelBtn.setOnAction(_ -> closeWindowConfirmation());
         confirmBtn.setOnAction(_ -> validateForm());
     }
 
     private void validateForm() {
-        String role = roleComboBox.getValue();
-        String department = departmentComboBox.getValue();
-
-        if (role == null || role.isEmpty() || department == null || department.isEmpty()) {
-            modalUtils.showModal(Modal.ERROR, "Failed", "Please select values for both fields.");
-        } else {
-            modalUtils.showModal(Modal.DEFAULT_APPROVE, "Confirm", "Are you sure you want to hire this employee? An email notification will be sent.", result -> {
-                if (result) updateAccount();
-            });
-        }
+        ComboBox[] comboBoxes = {departmentComboBox, roleComboBox};
+        if (validator.hasEmptyComboBox(comboBoxes)) return;
+        updateAccount();
     }
 
     private void updateAccount() {
@@ -70,12 +69,12 @@ public class SetupAccountController implements BaseViewController {
 
         Runnable call = () -> {
             try {
-                Response response = employeeService.activateEmployee(employeeId, selectedDepartment, RoleType.fromName(roleComboBox.getValue()));
+                Response response = employeeService.activateEmployee(residentId, selectedDepartment, RoleType.fromName(roleComboBox.getValue()));
                 Platform.runLater(() -> {
                     rootContainer.getChildren().remove(loadingIndicator);
                     if (response.isSuccessful()) {
                         closeWindow();
-                        modalUtils.showModal(Modal.SUCCESS, "Success", "Employee + " + employeeId + " has been successfully evaluated.");
+                        modalUtils.showModal(Modal.SUCCESS, "Success", "Applicant " + residentId + " has been successfully evaluated.");
                     } else {
                         closeWindow();
                         modalUtils.showModal(Modal.ERROR, "Failed", "An error occurred while activating employee application.");
@@ -106,7 +105,8 @@ public class SetupAccountController implements BaseViewController {
         departmentComboBox.setItems(FXCollections.observableArrayList(departmentNames));
     }
 
-    private void setupDepartmentComboBoxListener() {
+    private void setupEventListener() {
+        List<ComboBox<String>> comboBoxes = List.of(departmentComboBox, roleComboBox);
         departmentComboBox.setOnAction(_ -> {
             String selectedDepartmentName = departmentComboBox.getValue();
             selectedDepartment = getDepartmentByName(selectedDepartmentName);
@@ -114,6 +114,8 @@ public class SetupAccountController implements BaseViewController {
                 populateRoleComboBox(selectedDepartment);
             }
         });
+        validator.setupComboBox(comboBoxes);
+
     }
 
     private void populateRoleComboBox(DepartmentType departmentType) {
@@ -139,7 +141,6 @@ public class SetupAccountController implements BaseViewController {
         modalUtils.closeCustomizeModal();
     }
 
-    @FXML
     private void closeWindowConfirmation() {
         modalUtils.showModal(Modal.DEFAULT_REJECT, "Confirm Exit", "Are you sure you want to exit this window? Any unsaved changes will be lost.", result -> {
             if (result) closeWindow();
@@ -148,6 +149,6 @@ public class SetupAccountController implements BaseViewController {
 
     @Override
     public void setId(String id) {
-        this.employeeId = id;
+        this.residentId = id;
     }
 }
