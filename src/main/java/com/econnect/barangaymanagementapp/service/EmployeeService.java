839791245d16c2fbeb1192b7f392fb1fd2de7c37
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-11-17 22:44:24 -0800

    refactor: improve mailing

--- src/main/java/com/econnect/barangaymanagementapp/service/EmployeeService.java
+++ src/main/java/com/econnect/barangaymanagementapp/service/EmployeeService.java
@@ -35,10 +35,6 @@ public class EmployeeService {
         return employeeRepository.createEmployee(employee);
     }
 
-    public List<Employee> findAllEmployees() {
-        return employeeRepository.findAllEmployees();
-    }
-
     public Optional<Employee> findEmployeeById(String employeeId) {
         return employeeRepository.findEmployeeById(employeeId);
     }
@@ -59,17 +55,8 @@ public class EmployeeService {
                 .findFirst();
     }
 
-    public List<Employee> findAllEmployeesByStatus(StatusType.EmployeeStatus status) {
-        return employeeRepository.findEmployeeByFilter(employee -> employee.getStatus().equals(status));
-    }
-
     public List<Employee> findAllActiveEmployees() {
         return employeeRepository.findEmployeeByFilter(employee -> employee.getStatus().equals(ACTIVE));
-    }
-
-    public List<Employee> findAllOnlineApplicants() {
-        return employeeRepository.findEmployeeByFilter(employee ->
-                APPLICANTS_STATUSES.contains(employee.getStatus()) && employee.getApplicationType().equals(ApplicationType.ONLINE));
     }
 
     public List<Employee> findAllApplicants() {
@@ -77,30 +64,13 @@ public class EmployeeService {
                 APPLICANTS_STATUSES.contains(employee.getStatus()));
     }
 
-    public String findEmployeEmailById(String employeeId) {
-        return findEmployeeById(employeeId).map(employee -> employee.getEmail()).orElse(null);
-    }
-
-
     public int countAllActiveEmployees() {
         return findAllActiveEmployees().size();
     }
 
     public Response updateEmployeeToEvaluation(String employeeId) {
+        sendEvaluationEmail(findEmployeeById(employeeId).get());
         return updateEmployeeByStatus(employeeId, EVALUATION);
-    }
-
-    public Response updateEmployeeToEvaluation(String employeeId, String clearanceLink, String expirationDate) {
-        Optional<Employee> employee = this.findEmployeeById(employeeId);
-
-        if (employee.isPresent()) {
-            Employee updatedEmployee = employee.get();
-            updatedEmployee.setNbiClearanceUrl(clearanceLink);
-            updatedEmployee.setNbiClearanceExpiration(expirationDate);
-            updatedEmployee.setStatus(EVALUATION);
-            return employeeRepository.updateEmployee(updatedEmployee);
-        }
-        return null;
     }
 
     public Response activateEmployee(String employeeId, DepartmentType department, RoleType role) {
@@ -111,30 +81,7 @@ public class EmployeeService {
                 employee.setStatus(ACTIVE);
                 employee.setDepartment(department);
                 employee.setRole(role);
-                emailService.sendEmailAsync(employee.getEmail(), "Congratulations! You Have Been Hired", String.format("""
-                                Dear %s,
-
-                                We are thrilled to inform you that you have successfully completed our hiring process and have been officially hired to join our team.
-                                                            
-                                Your assigned role will be as %s in the %s department. We believe your skills and experiences will be a valuable asset to the team.
-                                                            
-                                Please report to our office at Old Capitol Site on [Insert Date] to finalize the onboarding process. If you have any questions, feel free to reach out to our HR team.
-                                                            
-                                Welcome aboard! We are excited to have you as part of our growing team.
-                                                                
-                                Please do not share this username and password to anyone.
-                                Username: %s
-                                Password: %s
-                                                            
-                                Best regards,
-                                Old Capitol Site
-                                """,
-                        employee.getFirstName(),
-                        role.getName(),
-                        department.getName(),
-                        employee.getUsername(),
-                        password
-                ));
+                sendActivationEmail(employee, department, role);
                 return employeeRepository.updateEmployee(employee);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -144,35 +91,10 @@ public class EmployeeService {
     }
 
     public Response updateEmployeeToUnderReview(String employeeId) {
-        Optional<Employee> response = findEmployeeById(employeeId);
-        Employee employee = response.get();
-        if (response.isPresent()) {
-            try {
-                emailService.sendEmailAsync(employee.getEmail(), "Congratulations! Your Application is Under Review", String.format("""
-                                Dear %s,
-
-                                    We are writing to inform you that your application has been carefully reviewed and we are pleased to announce that it has been accepted for further consideration.
-                                                                
-                                To proceed with the next steps in our hiring process, we kindly request that you visit our office located at Old Capitol Site. During this visit, you will be required to submit the necessary documents and complete any remaining application requirements.
-                                                                
-                                We look forward to meeting you and learning more about your qualifications.
-                                                
-                                Best regards,
-                                Old Capitol Site                
-                                """,
-                        employee.getFirstName()
-                ));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
         return updateEmployeeByStatus(employeeId, UNDER_REVIEW);
     }
 
     public Response terminateEmployee(String employeeId) {
-        Optional<Employee> findEmployee = findEmployeeById(employeeId);
-        if (!findEmployee.isPresent()) return null;
         return updateEmployeeByStatus(employeeId, TERMINATED);
     }
 
@@ -182,32 +104,9 @@ public class EmployeeService {
         Employee employee = findEmployee.get();
         Response response = updateEmployeeByStatus(employeeId, REJECTED);
         if (response.isSuccessful()) {
-            emailService.sendEmailAsync(employee.getEmail(), "We're sorry, Your Application is rejected", String.format("""
-                            Dear %s,
-                                We regret to inform you that your application will not be moving forward at this time.
-                                                                         
-                            We appreciate your interest in joining our team and encourage you to reapply for future opportunities that align with your skills and experiences.
-                                                                         
-                            Best regards,
-                            Old Capitol Site
-                                     """,
-                    employee.getFirstName()
-            ));
+            sendRejectionEmail(employee);
         }
         return response;
-    }
-
-    private Response updateEmployeeByStatusDepartmentRole(String employeeId, StatusType.EmployeeStatus status, DepartmentType department, RoleType role) {
-        Optional<Employee> response = findEmployeeById(employeeId);
-
-        if (response.isPresent()) {
-            Employee employee = response.get();
-            employee.setStatus(status);
-            employee.setDepartment(department);
-            employee.setRole(role);
-            return employeeRepository.updateEmployee(employee);
-        }
-        return employeeRepository.updateEmployeeByStatus(employeeId, status);
     }
 
     public Response updateEmployeeByStatus(String employeeId, StatusType.EmployeeStatus status) {
@@ -217,11 +116,12 @@ public class EmployeeService {
     private Employee generateEmployeeUsernameAndPassword(Employee employee) {
         int employeeCount = countAllActiveEmployees() + 1;
         String formattedId = String.format("%03d", employeeCount);
-        String username = employee.getLastName().toLowerCase() + formattedId;
-        password = employee.getLastName().toLowerCase() + generate6DigitPin();
-//        String hashedPassword = passwordUtils.encryptPassword(password);
+        String sanitizedLastName = employee.getLastName().toLowerCase().replaceAll("\\s+", "");
+        String username = sanitizedLastName + formattedId;
+        password = sanitizedLastName + generate6DigitPin();
         employee.setUsername(username);
         employee.setPassword(password);
+
         return employee;
     }
 
@@ -235,4 +135,64 @@ public class EmployeeService {
         employeeRepository.enableLiveReload(handleDataUpdate);
     }
 
+    private void sendActivationEmail(Employee employee, DepartmentType department, RoleType role) {
+        emailService.sendEmailAsync(employee.getEmail(), "Congratulations! You Have Been Hired", String.format("""
+                        Dear %s,
+
+                        We are thrilled to inform you that you have successfully completed our hiring process and have been officially hired to join our team.
+                                                    
+                        Welcome aboard! We are excited to have you as part of our growing team.                            
+                        Your assigned role will be as %s in the %s department. We believe your skills and experiences will be a valuable asset to the team.
+                                                    
+                        Please report to our office at Old Capitol Site to finalize the onboarding process. If you have any questions, feel free to reach out to our HR team.
+                                                        
+                        Please do not share this username and password to anyone.
+                        Username: %s
+                        Password: %s
+                                                    
+                        Best regards,
+                        Old Capitol Site
+                        """,
+                employee.getFirstName(),
+                role.getName(),
+                department.getName(),
+                employee.getUsername(),
+                password
+        ));
+    }
+
+    private void sendEvaluationEmail(Employee employee) {
+        try {
+            emailService.sendEmailAsync(employee.getEmail(), "Congratulations! Your Application has been accepted", String.format("""
+                            Dear %s,
+
+                                We are writing to inform you that your application has been carefully reviewed and we are pleased to announce that it has been accepted for further consideration.
+                                                            
+                            To proceed with the next step, we kindly request you to visit our office located at Old Capitol Site to process you interview.
+                                                            
+                            We look forward to meeting you and learning more about your qualifications.
+                                            
+                            Best regards,
+                            Old Capitol Site                
+                            """,
+                    employee.getFirstName()
+            ));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendRejectionEmail(Employee employee) {
+        emailService.sendEmailAsync(employee.getEmail(), "We're sorry, Your Application is rejected", String.format("""
+                        Dear %s,
+                            We regret to inform you that your application will not be moving forward at this time.
+                                                                     
+                        We appreciate your interest in joining our team and encourage you to reapply for future opportunities that align with your skills and experiences.
+                                                                     
+                        Best regards,
+                        Old Capitol Site
+                                 """,
+                employee.getFirstName()
+        ));
+    }
 }
