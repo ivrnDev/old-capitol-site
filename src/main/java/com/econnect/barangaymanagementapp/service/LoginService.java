1938e096062392c56a9de32f4694799a702f0228
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-11-23 21:06:20 -0800

    feat: add employee image dynamically on header

--- src/main/java/com/econnect/barangaymanagementapp/service/LoginService.java
+++ src/main/java/com/econnect/barangaymanagementapp/service/LoginService.java
@@ -1,24 +1,39 @@
 package com.econnect.barangaymanagementapp.service;
 
+import com.econnect.barangaymanagementapp.MainApplication;
 import com.econnect.barangaymanagementapp.domain.Employee;
+import com.econnect.barangaymanagementapp.enumeration.database.Firebase;
+import com.econnect.barangaymanagementapp.enumeration.database.Firestore;
+import com.econnect.barangaymanagementapp.enumeration.path.FXMLPath;
 import com.econnect.barangaymanagementapp.util.DependencyInjector;
 import com.econnect.barangaymanagementapp.util.state.UserSession;
+import javafx.scene.image.Image;
 
+import java.util.Objects;
 import java.util.Optional;
+
+import static com.econnect.barangaymanagementapp.enumeration.path.FXMLPath.DEFAULT_PROFILE;
 
 public class LoginService {
     private final EmployeeService employeeService;
     private final UserSession userSession;
+    private final ImageService imageService;
 
     public LoginService(DependencyInjector dependencyInjector) {
         this.employeeService = dependencyInjector.getEmployeeService();
         this.userSession = dependencyInjector.getUserSession();
+        this.imageService = dependencyInjector.getImageService();
     }
 
     public Optional<Employee> login(String username, String password) {
         Optional<Employee> employee = employeeService.findEmployeeByCredentials(username, password);
         if (employee != null && employee.isPresent()) {
             userSession.setSession(employee.get());
+            if (employee.get().getProfileUrl() != null) {
+                userSession.setEmployeeImage(imageService.getImage(Firestore.PROFILE_PICTURE.getPath(), employee.get().getProfileUrl()));
+            } else {
+                userSession.setEmployeeImage(new Image(Objects.requireNonNull(MainApplication.class.getResourceAsStream(DEFAULT_PROFILE.getFxmlPath()))));
+            }
             return employee;
         }
         return Optional.empty();
--- src/main/java/com/econnect/barangaymanagementapp/util/state/UserSession.java
+++ src/main/java/com/econnect/barangaymanagementapp/util/state/UserSession.java
@@ -3,13 +3,19 @@ package com.econnect.barangaymanagementapp.util.state;
 import com.econnect.barangaymanagementapp.domain.Employee;
 import com.econnect.barangaymanagementapp.enumeration.type.DepartmentType;
 import com.econnect.barangaymanagementapp.enumeration.type.RoleType;
+import javafx.scene.image.Image;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@NoArgsConstructor
 public class UserSession {
     private static UserSession instance;
     private Employee currentEmployee;
-
-    private UserSession() {
-    }
+    @Setter
+    private Image employeeImage;
 
     public static UserSession getInstance() {
         if (instance == null) {
@@ -20,10 +26,6 @@ public class UserSession {
 
     public void setSession(Employee employee) {
         this.currentEmployee = employee;
-    }
-
-    public Employee getCurrentEmployee() {
-        return currentEmployee;
     }
 
     public DepartmentType getEmployeeDepartment() {
