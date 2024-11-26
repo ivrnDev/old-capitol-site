a75088aeebe105e0957053ba7fdb241a1a5b59ed
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-11-26 05:35:57 -0800

    refactor: make the stock level as enum

--- src/main/java/com/econnect/barangaymanagementapp/util/StockLevel.java
+++ src/main/java/com/econnect/barangaymanagementapp/util/StockLevel.java
@@ -3,9 +3,20 @@ package com.econnect.barangaymanagementapp.util;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-public class StockLevel {
+@AllArgsConstructor
+@Getter
+public enum StockLevel {
+    OUT_OF_STOCK("Out of Stock"),
+    CRITICAL("Critical"),
+    SAFETY_STOCK("Safety Stock"),
+    LOW("Low"),
+    MEDIUM("Medium"),
+    MEDIUM_HIGH("Medium High"),
+    HIGH("High");
+    private final String name;
 
-    public static Level getLevel(int currentStocks, int minStocks, int maxStocks) {
+
+    public static StockLevel getLevel(int currentStocks, int minStocks, int maxStocks) {
         if (maxStocks <= minStocks) {
             throw new IllegalArgumentException("Max stocks must be greater than min stocks");
         }
@@ -14,15 +25,15 @@ public class StockLevel {
         int effectiveMinStocks = minStocks + safetyStock;  // Effective minimum stock is minStocks + safetyStock
 
         if (currentStocks == 0) {
-            return Level.OUT_OF_STOCK;
+            return StockLevel.OUT_OF_STOCK;
         }
 
         if (currentStocks <= effectiveMinStocks && currentStocks > minStocks) {
-            return Level.SAFETY_STOCK;
+            return StockLevel.SAFETY_STOCK;
         }
 
         if (currentStocks <= effectiveMinStocks) {
-            return Level.CRITICAL;
+            return StockLevel.CRITICAL;
         }
 
         // Calculate percentage-based levels for remaining stocks.
@@ -31,27 +42,22 @@ public class StockLevel {
         int percentage = (adjustedStocks * 100) / range;
 
         if (percentage <= 25) {
-            return Level.LOW;
+            return StockLevel.LOW;
         } else if (percentage <= 50) {
-            return Level.MEDIUM;
+            return StockLevel.MEDIUM;
         } else if (percentage <= 75) {
-            return Level.MEDIUM_HIGH;
+            return StockLevel.MEDIUM_HIGH;
         } else {
-            return Level.HIGH;
+            return StockLevel.HIGH;
         }
     }
 
-    @AllArgsConstructor
-    @Getter
-    public enum Level {
-        OUT_OF_STOCK("Out of Stock"),
-        CRITICAL("Critical"),
-        SAFETY_STOCK("Safety Stock"),
-        LOW("Low"),
-        MEDIUM("Medium"),
-        MEDIUM_HIGH("Medium High"),
-        HIGH("High");
-
-        private final String name;
+    public static StockLevel fromName(String name) {
+        for (StockLevel level : StockLevel.values()) {
+            if (level.getName().equals(name)) {
+                return level;
+            }
+        }
+        return null;
     }
 }
