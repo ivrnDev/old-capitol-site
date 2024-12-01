256302e53c4e038707c6eb492ed969e28464f6cb
ivanren <villamora.ivanren.manguiat@gmail.com>
2024-12-01 02:14:56 -0800

    refactor: rename item path

--- src/main/java/com/econnect/barangaymanagementapp/enumeration/database/Firestore.java
+++ src/main/java/com/econnect/barangaymanagementapp/enumeration/database/Firestore.java
@@ -7,7 +7,7 @@ public enum Firestore {
     ANNOUNCEMENT("/announcement_images", "announcement_images"),
     RESUME("/3-Resume", "Resume"),
     NBI_CLEARANCE("/3-NBIClearance", "NBIClearance"),
-    ITEM("/Item", "Item");
+    ITEM("/3-Item", "Item");
     public final String path;
     public final String name;
 
