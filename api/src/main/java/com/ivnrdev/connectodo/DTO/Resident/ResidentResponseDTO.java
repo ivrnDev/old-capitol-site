ebaa653cbdf337855a8c1ac1e42e0b90cbbd035c
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-03-06 03:51:45 -0800

    Create Resident request and response DTO

---
+++ api/src/main/java/com/ivnrdev/connectodo/DTO/Resident/ResidentResponseDTO.java
@@ -0,0 +1,67 @@
+package com.ivnrdev.connectodo.DTO.Resident;
+
+import com.ivnrdev.connectodo.Enums.ResidentEnums;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class ResidentResponseDTO {
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffixName;
+    private String mobileNumber;
+    private String telephoneNumber;
+    private String email;
+    private String address;
+    private String sex;
+    private String birthdate;
+    private String birthplace;
+    private String citizenship;
+    private String occupation;
+    private ResidentEnums.CivilStatus civilStatus;
+    private String motherTounge;
+    private ResidentEnums.BloodType bloodType;
+    private String religion;
+
+    private String fatherFirstName;
+    private String fatherLastName;
+    private String fatherMiddleName;
+    private String fatherSuffixName;
+    private String fatherOccupation;
+
+    private String motherFirstName;
+    private String motherLastName;
+    private String motherMiddleName;
+    private String motherSuffixName;
+    private String motherOccupation;
+
+    private String spouseFirstName;
+    private String spouseLastName;
+    private String spouseMiddleName;
+    private String spouseSuffixName;
+    private String spouseOccupation;
+
+    private String emergencyFirstName;
+    private String emergencyLastName;
+    private String emergencyMiddleName;
+    private String emergencyMobileNumber;
+    private String emergencyRelationship;
+
+    private String sourceOfIncome;
+    private ResidentEnums.ResidencyStatus residencyStatus;
+    private ResidentEnums.ResidentStatus status;
+    private String educationalAttainment;
+    private String profileUrl;
+    private String validIdUrl;
+    private String validIdExpiration;
+    private String tinIdUrl;
+    private String tinIdNumber;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
