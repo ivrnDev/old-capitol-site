20d70f5a2343f1300a4606036f7d1c5921799232
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-04-11 18:12:17 -0700

    Add repository annotation

--- api/src/main/java/com/ivnrdev/connectodo/Repository/CedulaRepository.java
+++ api/src/main/java/com/ivnrdev/connectodo/Repository/CedulaRepository.java
@@ -2,6 +2,8 @@ package com.ivnrdev.connectodo.Repository;
 
 import com.ivnrdev.connectodo.Domain.Cedula;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CedulaRepository extends CrudRepository<Cedula, Long> {
 }
